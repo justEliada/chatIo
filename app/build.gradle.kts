101efeafaafcb747c803c56a2123b20aae156f7e
justEliada <eliadaballazhi@gmail.com>
2024-03-19 14:07:42 +0100

    some file changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures{
+        viewBinding = true // code that easily interacts with views
+    }
+
 }
 
 dependencies {
@@ -37,7 +41,15 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    
+
+    //Scalable Size(for different screen sizes)
+    implementation ("com.intuit.sdp:sdp-android:1.0.6")
+    implementation ("com.intuit.ssp:ssp-android:1.0.6")
+
+    //Rounded ImageView
+    implementation("com.makeramen:roundedimageview:2.3.0")
+
+
     //Firebase
     implementation(platform("com.google.firebase:firebase-bom:32.7.4"))
     implementation("com.google.firebase:firebase-analytics")
--- app/src/main/java/com/example/chatio/activities/register.java
+++ app/src/main/java/com/example/chatio/activities/register.java
@@ -2,15 +2,30 @@ package com.example.chatio.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.chatio.R;
+import com.example.chatio.databinding.ActivityRegisterBinding;
 
 public class register extends AppCompatActivity {
+
+    private ActivityRegisterBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_register);
+        setListeners();
     }
+
+
+    private void setListeners() {
+        binding.RegisterButtonRegisterScreen.setOnClickListener(
+                v -> onBackPressed());
+
+    }
+
 }
