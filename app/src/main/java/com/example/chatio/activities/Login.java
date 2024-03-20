01abf87ad10d35f02c8ce7a51b7070146823457c
justEliada <eliadaballazhi@gmail.com>
2024-03-20 15:29:17 +0100

    login  validations & firestore additions

--- app/src/main/java/com/example/chatio/activities/Login.java
+++ app/src/main/java/com/example/chatio/activities/Login.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 import com.example.chatio.databinding.ActivityLoginBinding;
+import com.example.chatio.utilitis.Constants;
+import com.example.chatio.utilitis.PreferenceManager;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -13,10 +16,12 @@ import java.util.HashMap;
 public class Login extends AppCompatActivity {
 
     private ActivityLoginBinding binding;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        preferenceManager = new PreferenceManager(getApplicationContext());
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setListeners();
@@ -25,10 +30,59 @@ public class Login extends AppCompatActivity {
     private void setListeners(){
         binding.RegisterButton.setOnClickListener(v->
                 startActivity(new Intent(getApplicationContext(), Register.class)));
-        binding.LoginButton.setOnClickListener(v->addDataToFirestore());
+        binding.LoginButton.setOnClickListener(v->{
+            if(isValidCredentials()){
+                login();
+            }
+        });
     }
 
-    private void addDataToFirestore(){
+    private void showToast(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void login(){
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .whereEqualTo(Constants.KEY_NAME, binding.UserName.getText().toString())
+                .whereEqualTo(Constants.KEY_PASSWORD, binding.Password.getText().toString())
+                .get()
+                .addOnCompleteListener(task -> {
+                   if(task.isSuccessful() && task.getResult() != null &&
+                   task.getResult().getDocuments().size()>0){
+                       DocumentSnapshot documentSnapshot = task.getResult().getDocuments().get(0);
+                       preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN, true);
+                       preferenceManager.putString(Constants.KEY_USER_ID, documentSnapshot.getId());
+                       Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                       intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                       startActivity(intent);
+                   }
+                   else{
+                       showToast("Unable to Login");
+                   }
+                });
+
+    }
+
+    private Boolean isValidCredentials()
+    {
+        if(binding.UserName.getText().toString().trim().isEmpty()){
+        showToast("Enter Username");
+        return false;
+
+        }
+        else if(binding.Password.getText().toString().trim().isEmpty()){
+        showToast("Enter Password");
+        return false;
+
+        }
+        else {
+            return true;
+        }
+    }
+
+    // testing the connection is working
+/*    private void addDataToFirestore(){
         FirebaseFirestore database = FirebaseFirestore.getInstance();
         HashMap<String, Object> data = new HashMap<>();
         data.put("first_name", "Eliada");
@@ -41,7 +95,7 @@ public class Login extends AppCompatActivity {
                 .addOnFailureListener(exception -> {
                     Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_SHORT).show();
                 });
-    }
+    }*/
 
 
 
